3a35bc2f49606304f2846ae371924106d31305bc
Kata1213 <2726456039@qq.com>
2018-05-03 22:09:46 +0800

    first create

---
+++ src/main/java/com/TW/Homework/Kata1213/SpringBoot/demo/week6_SpringBoot/Week6SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.TW.Homework.Kata1213.SpringBoot.demo.week6_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week6SpringBootApplication.class, args);
+	}
+}
